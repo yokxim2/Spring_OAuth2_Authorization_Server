fa43a012985ac3516d7fe4d3b26700d88c0007e3
yokxim <yooksi53@gmail.com>
2025-02-06 10:59:10 +0900

    fix[security-config] AuthorizationServerSettings 커스텀 설정과 다중 SecurityFilterChain 등록

--- src/main/java/org/example/spring_oauth2_authorization_server/config/SecurityConfig.java
+++ src/main/java/org/example/spring_oauth2_authorization_server/config/SecurityConfig.java
@@ -2,10 +2,18 @@ package org.example.spring_oauth2_authorization_server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -32,5 +40,41 @@ public class SecurityConfig {
                 .formLogin(withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authorizationServer(HttpSecurity http) throws Exception {
+
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        http
+                .getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(withDefaults());
+        http
+                .exceptionHandling((exceptions) -> exceptions
+                        .defaultAuthenticationEntryPointFor(
+                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                        )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+
+        return AuthorizationServerSettings.builder()
+                .issuer("http://localhost:9000")
+                .authorizationEndpoint("/oauth2/v1/authorize")
+                .tokenEndpoint("/oauth2/v1/token")
+                .tokenIntrospectionEndpoint("/oauth2/v1/introspect") // 토큰 상태
+                .tokenRevocationEndpoint("/oauth2/v1/revoke") // 토큰 폐기 RFC 7009
+                .jwkSetEndpoint("/oauth2/v1/jwks") // 공개키 확인
+                .oidcLogoutEndpoint("/connect/v1/logout")
+                .oidcUserInfoEndpoint("/connect/v1/userinfo") // 리소스 서버 유저 정보 연관
+                .oidcClientRegistrationEndpoint("/connect/v1/register") // OAuth2 사용 신청
+                .build();
     }
 }
