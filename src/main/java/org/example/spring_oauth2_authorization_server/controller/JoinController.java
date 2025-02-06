c77e622976d1bed8326b971846aa13e30362550a
yokxim <yooksi53@gmail.com>
2025-02-06 10:44:44 +0900

    feat[join-controller] 회원가입 컨트롤러 클래스 추가

--- src/main/java/org/example/spring_oauth2_authorization_server/controller/JoinController.java
+++ src/main/java/org/example/spring_oauth2_authorization_server/controller/JoinController.java
@@ -1,4 +1,34 @@
 package org.example.spring_oauth2_authorization_server.controller;
 
+import org.example.spring_oauth2_authorization_server.dto.UserDTO;
+import org.example.spring_oauth2_authorization_server.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class JoinController {
+
+    private final UserService userService;
+
+    public JoinController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/join")
+    public String joinPage() {
+
+        return "joinPage";
+    }
+
+    @PostMapping("/join")
+    @ResponseBody
+    public String join(UserDTO dto) {
+
+        userService.join(dto);
+
+        return "ok";
+    }
 }
+
