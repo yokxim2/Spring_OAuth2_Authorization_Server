6c901dd4e7992d02798eecf5d4c8aeaa91a0b43c
yokxim <yooksi53@gmail.com>
2025-02-06 10:46:43 +0900

    feat[custom-userDetails-service] UserDetailsService 구현체 추가
    - OAuth2 Authorization 의존성을 추가하면 추가로 SecurityJackson2 인터페이스가 같이 딸려오는데, 이 때문에 UserDetails를 커스텀 해서 만들 수 없다. 꼭 User.Builder를 사용해서 유저 객체를 만들어야 한다.

--- src/main/java/org/example/spring_oauth2_authorization_server/service/CustomUserDetailsService.java
+++ src/main/java/org/example/spring_oauth2_authorization_server/service/CustomUserDetailsService.java
@@ -1,4 +1,33 @@
 package org.example.spring_oauth2_authorization_server.service;
 
-public class UserDetailsService {
+import org.example.spring_oauth2_authorization_server.entity.UserEntity;
+import org.example.spring_oauth2_authorization_server.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity entity = userRepository.findByUsername(username).orElseThrow();
+
+        UserDetails userDetails = User.builder()
+                .username(entity.getUsername())
+                .password(entity.getPassword())
+                .roles(entity.getRole())
+                .build();
+
+        return userDetails;
+    }
 }
