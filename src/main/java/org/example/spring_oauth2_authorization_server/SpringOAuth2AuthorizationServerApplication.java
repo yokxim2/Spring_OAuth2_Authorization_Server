a4cd2cafec9483970df8c0394bfd6c29cfe63bdb
yokxim <yooksi53@gmail.com>
2025-02-06 09:58:50 +0900

    Initial commit

---
+++ src/main/java/org/example/spring_oauth2_authorization_server/SpringOAuth2AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_oauth2_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuth2AuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOAuth2AuthorizationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spring_oauth2_authorization_server/SpringOAuth2AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_oauth2_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuth2AuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
