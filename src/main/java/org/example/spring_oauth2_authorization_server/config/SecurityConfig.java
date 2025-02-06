acb9f6ca8dd778bccfba5b25dd09febee0ba645b
yokxim <yooksi53@gmail.com>
2025-02-06 12:06:29 +0900

    fix[security-config] 불필요한 import 구문 삭제

--- src/main/java/org/example/spring_oauth2_authorization_server/config/SecurityConfig.java
+++ src/main/java/org/example/spring_oauth2_authorization_server/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.web.SecurityFilterChain;
