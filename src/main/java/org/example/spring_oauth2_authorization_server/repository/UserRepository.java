96f3528e20917e0a7eff892f8fcc56a3b3501522
yokxim <yooksi53@gmail.com>
2025-02-06 10:43:04 +0900

    fix[.gitignore] 오타 수정

--- src/main/java/org/example/spring_oauth2_authorization_server/repository/UserRepository.java
+++ src/main/java/org/example/spring_oauth2_authorization_server/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package org.example.spring_oauth2_authorization_server.repository;
 
-public class UserRepository {
+import org.example.spring_oauth2_authorization_server.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
 }
