bb5b26a973b5a3f94438f9f785cb56987cefddcb
yokxim <yooksi53@gmail.com>
2025-02-06 11:22:04 +0900

    feat[register-repository] Register 엔티티 저장소 클래스 추가

---
+++ src/main/java/org/example/spring_oauth2_authorization_server/repository/RegisterRepository.java
@@ -0,0 +1,10 @@
+package org.example.spring_oauth2_authorization_server.repository;
+
+import org.example.spring_oauth2_authorization_server.entity.RegisterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RegisterRepository extends JpaRepository<RegisterEntity, String> {
+    Optional<RegisterEntity> findByClientId(String clientId);
+}
