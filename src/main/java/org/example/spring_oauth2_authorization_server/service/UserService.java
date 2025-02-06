f5706c478c540b0514507e1238b8585d03c85dec
yokxim <yooksi53@gmail.com>
2025-02-06 10:43:38 +0900

    feat[user-service] 유저 서비스 클래스 추가

--- src/main/java/org/example/spring_oauth2_authorization_server/service/UserService.java
+++ src/main/java/org/example/spring_oauth2_authorization_server/service/UserService.java
@@ -1,4 +1,31 @@
 package org.example.spring_oauth2_authorization_server.service;
 
+import org.example.spring_oauth2_authorization_server.dto.UserDTO;
+import org.example.spring_oauth2_authorization_server.entity.UserEntity;
+import org.example.spring_oauth2_authorization_server.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void join(UserDTO dto) {
+
+        UserEntity entity = new UserEntity();
+        entity.setUsername(dto.getUsername());
+        entity.setPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
+        entity.setNickname(dto.getUsername());
+        entity.setPhone(dto.getPhone());
+        entity.setRole("ADMIN");
+
+        userRepository.save(entity);
+    }
 }
